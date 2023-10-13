811b8314649418b163c7b16c49af3ddadd3e45c0
AliAkbarSobhanpour <a.a.s.javid0416@gmail.com>
2023-10-13 14:00:48 +0330

    installing django-ckeditor

--- CV/models.py
+++ CV/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    pass
+    address = RichTextField("address")
+    position_name = models.CharField("position name", max_length=200, help_text="maximum 25 char")
+    
+    
 
 
 
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CV.apps.CvConfig'
+    # internal apps 
+    'CV.apps.CvConfig',
+    
+    # external apps 
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
