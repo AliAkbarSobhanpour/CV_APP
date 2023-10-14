6c320a5ac8e16ce02abc4b404d9a37db29317882
AliAkbarSobhanpour <a.a.s.javid0416@gmail.com>
2023-10-14 17:49:12 +0330

    change template part1

--- CV/views.py
+++ CV/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import CustomUser
 
 # Create your views here.
 
 def CV_view(request):
     
+    user = CustomUser.objects.all()[0]
 
-    context = {}
+    context = {"user":user}
 
     return render(request, "CV/index.html",context)
---
+++ static/scripts/custom.js
@@ -0,0 +1,18 @@
+const toggleButtons = document.querySelectorAll('.toggle-button');
+const hiddenImages = document.querySelectorAll('.hidden-image');
+const fullscreenImage = document.getElementById('fullscreen-image');
+const fullscreenImageContent = document.getElementById('fullscreen-image-content');
+
+toggleButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Set the source of the full-screen image
+        fullscreenImageContent.src = hiddenImages[index].src;
+        // Display the full-screen image
+        fullscreenImage.style.display = 'block';
+    });
+});
+
+// Function to close the full-screen image
+function closeFullscreen() {
+    fullscreenImage.style.display = 'none';
+}
